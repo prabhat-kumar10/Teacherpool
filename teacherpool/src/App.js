17986785ad334df980c77ede8bb4dc580ea1d9c8
prabhat-kumar10 <prabhatk.std@gmail.com>
2023-12-12 21:50:04 +0530

    Created basic layout

--- teacherpool/src/App.js
+++ teacherpool/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Contact from "./screens/Contact";
+import Register from "./screens/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ teacherpool/src/screens/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const Register = () => {
+  return (
+    <>
+        <NavBar />
+        <div>Register</div>
+    </>
+  )
+}
+
+export default Register
