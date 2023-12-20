6e43d2b74bb09b87812d8a40eee8a9284ebd0f03
prabhat-kumar10 <prabhatk.std@gmail.com>
2023-12-20 18:53:11 +0530

    fixed somethings

--- api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const api = "http://localhost:3000/"
-
-const Api = axios.create({
- baseURL: api,
-});
-Api.defaults.headers.post['Content-Type'] = 'application/json';
-
-export default Api;
