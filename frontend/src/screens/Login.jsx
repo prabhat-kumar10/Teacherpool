b1cd19bb4331a759f27dd69293553efbf0acefd3
prabhat-kumar10 <prabhatk.std@gmail.com>
2024-01-04 12:36:33 +0530

    folder structure changed

---
+++ frontend/src/screens/Login.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/Login.css";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import axios from "axios";
+import { useAuth } from "../AuthContext";
+import Loginphoto from "../assets/Loginphoto.svg";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [allEntry, setallEntry] = useState([]);
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newEntry = { email: email, password: password };
+    setallEntry([...allEntry, newEntry]);
+
+    try {
+      const response = await axios.post("https://teacherpool-u1m9.onrender.com/login", {
+        email,
+        password,
+      });
+
+      const { message, user } = response.data;
+      // console.log(response.data);
+
+      if (message === "Login Successful") {
+        console.log("Login done successfully");
+        // alert("Login Succesfull");
+        login(user);
+        navigate("/");
+      } else {
+        alert("Login failed");
+        console.log(message);
+      }
+    } catch (error) {
+      console.log("Login error", error);
+    }
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div className="Logincontainer">
+        <img src={Loginphoto} alt="loginphoto" className="loginphoto" />
+        <form onSubmit={handleSubmit} action="/signup" method="POST">
+          <h1>Welcome to Teacher Pool</h1>
+          <h3>Sign In</h3>
+
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password">
+            Not signed up yet? <Link to="/signup">Sign Up</Link>
+          </p>
+        </form>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
---
+++ frontend/src/screens/Signup.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import "../styles/Signup.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Signupphoto from "../assets/Signupphoto.svg";
+
+const Signup = () => {
+  const [fullname, setfullname] = useState("");
+  const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword){
+      alert("Password did not match!");
+    }
+    else if (password.length < 8 || password.length > 15){
+      alert("Password length must be between 8 to 15!");
+    }
+    else if (phone.length !== 10) {
+      alert("Phone number must be of 10 digits!");
+    }
+    else {
+      try {
+        const response = await axios.post("https://teacherpool-u1m9.onrender.com/signup", {
+          fullname,
+          phone,
+          email,
+          password,
+        });
+        console.log(response.data.email);
+        alert("Signed Up Successfully! Login now");
+        navigate("/login");
+      } catch (error) {
+        alert("Sign up error");
+        console.log("Sign up error", error);
+      }
+    }
+    setemail("");
+    setpassword("");
+    setConfirmPassword("");
+    setfullname("");
+    setphone("");
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div className="signup-container">
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <h1>Welcome to Teacher Pool</h1>
+          <h3 className="signup-title">Sign Up</h3>
+
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Full name"
+              name="fullname"
+              value={fullname}
+              onChange={(e) => setfullname(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="tel"
+              className="form-control"
+              placeholder="Enter Phone Number"
+              value={phone}
+              name="phone"
+              onChange={(e) => setphone(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter Email"
+              value={email}
+              name="email"
+              onChange={(e) => setemail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter Password"
+              value={password}
+              name="password"
+              onChange={(e) => setpassword(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              name="confirmPassword"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Sign Up
+            </button>
+          </div>
+          <p className="forgot-password">
+            Already registered? <Link to="/login">Login</Link>
+          </p>
+        </form>
+        <img src={Signupphoto} alt="Signupphoto" className="Signupphoto" />
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Signup;
